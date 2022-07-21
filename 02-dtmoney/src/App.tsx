95cab997cfa9917914995efc7c23eff4c8906c04
vitorTest <vytorfelix2@gmail.com>
2022-07-21 00:33:47 -0300

    feat: implements header html/css structure

--- 02-dtmoney/src/App.tsx
+++ 02-dtmoney/src/App.tsx
@@ -1,14 +1,12 @@
-import styled from 'styled-components';
+import {GlobalStyle} from "./styles/global"
 
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`;
+import {Header} from "./components/Header"
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Hello World</Title>
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   );
 }
